f95a96a8d0f0a42796db59f93f795f7ae07ed0ea
Sk Belal Saheb <skbelalsaheb@gmail.com>
2020-12-05 11:51:33 +0530

    Media File Updated

--- DjangoWeb/settings.py
+++ DjangoWeb/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR,"static")
 # ]
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static")
- ]
+     os.path.join(BASE_DIR, "static"),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL='/media/'
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 from home import views
 urlpatterns = [
     path('', views.Index.as_view()),
-]
+    ]
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
